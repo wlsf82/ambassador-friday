3bb3d30c22562c973269c32b04b21cd83e03eae0
Walmyr <wlsf82@gmail.com>
2026-01-09 22:41:42 +0100

    Update commands for simulation purposes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,23 @@
 Cypress.Commands.add('login', (environment, userType) => {
   setEnvironmentCredentials(environment)
 
-  cy.visit('/login')
+  // cy.visit('/login')
+  cy.log(`Visiting the ${Cypress.config('baseUrl')}/login page`)
 
   const email = Cypress.env('credentials')[userType].USER_EMAIL
   const password = Cypress.env('credentials')[userType].USER_PASSWORD
 
-  cy.get('input[placeholder="Email"]').type(email)
-  cy.get('input[placeholder="Password"]').type(password, { log: false })
-  cy.contains('button', 'Login').click()
+  // cy.get('input[placeholder="Email"]').type(email)
+  cy.log(`Typing the email: ${email}`)
+  // cy.get('input[placeholder="Password"]').type(password, { log: false })
+  cy.log('Typing the password')
+  // cy.contains('button', 'Login').click()
+  cy.log('Clicking the Login button')
 
-  cy.url().should('be.equal', `${Cypress.config('baseUrl')}/dashboard`)
-  cy.get('[data-testid="avatar"]').should('be.visible')
+  // cy.url().should('be.equal', `${Cypress.config('baseUrl')}/dashboard`)
+  cy.log(`Asserting the current URL is ${Cypress.config('baseUrl')}/dashboard`)
+  // cy.get('[data-testid="avatar"]').should('be.visible')
+  cy.log('Asserting the avatar is visible ensuring the user has logged in successfully')
 })
 
 Cypress.Commands.add('sessionLogin', (environment, userType) => {
@@ -24,7 +30,8 @@ Cypress.Commands.add('sessionLogin', (environment, userType) => {
   const sessionId = Cypress.env('credentials')[userType].USER_EMAIL
 
   cy.session(sessionId, setup)
-  cy.visit('/dashboard')
+  // cy.visit('/dashboard')
+  cy.log(`Visiting the ${Cypress.config('baseUrl')}/dashboard page`)
 })
 
 function setEnvironmentCredentials(environment) {
